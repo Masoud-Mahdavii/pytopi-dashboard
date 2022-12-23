b9d40686b22864a4f631abcbd338ba4ca38e0bed
masoud mahdavi <masoudmahdavi1993@gmail.com>
2022-12-23 22:32:45 +0330

    update4 run.py

--- src/run.py
+++ src/run.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import pandas as pd
+import json
 
+try:
+    from StringIO import StringIO 
+except ImportError:
+    from io import StringIO 
 
 st.title(':zap: Pytopia Dashboard')
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+    string_data = stringio.read()
+    st.write(string_data)
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+data = json.loads(string_data)
+st.json(data)
