652b7a27f26a1658c884e81fc46c161e249faffe
masoud mahdavi <masoudmahdavi1993@gmail.com>
2023-01-11 09:46:05 +0330

    updata run.py agi

--- src/run.py
+++ src/run.py
@@ -8,15 +8,11 @@ import streamlit as st
 st.title(':zap: Pytopia Dashboard')
 
 
-with st.expander('Statistics'):
-    fig, ax = pit.subplots(1, 1, figsize=(10, 5))
-    sns.histplots(np.random.randn(100), ax=ax)
-    st.pyplot(fig)
-
-
 
 with st.expander('User Profile:'):
-    st.text_input('Name:')
+    col1, col2 = st.colums(2)
+    col1.text_input('Name:')
+    col2.text_input('Location:')
     st.camera_input('camrta Input', key='camera_input')
 
 
