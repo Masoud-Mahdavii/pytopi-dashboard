8145d1895bf0b6ef282988e315d7470482a06572
masoud mahdavi <masoudmahdavi1993@gmail.com>
2023-01-11 09:16:36 +0330

    updata run.py second

--- src/run.py
+++ src/run.py
@@ -11,12 +11,5 @@ except ImportError:
 st.title(':zap: Pytopia Dashboard')
 
 with st.expander('Statistics'):
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(string_data)
-
-        data = json.loads(string_data)
-        st.json(data['messages'][0])
+    pass
 
