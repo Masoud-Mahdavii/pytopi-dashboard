ef4b29ac1e1da4a308efb3e07fb6b586df36e1a4
masoud mahdavi <masoudmahdavi1993@gmail.com>
2023-01-11 09:42:24 +0330

    updata run.py agin

--- src/run.py
+++ src/run.py
@@ -1,14 +1,22 @@
-import streamlit as std 
 import json
-import seaborn as sns
-import numpy as np
+
 import matplotlib.pyplot as pit
+import numpy as np
+import seaborn as sns
+import streamlit as st
 
 st.title(':zap: Pytopia Dashboard')
+
 
 with st.expander('Statistics'):
     fig, ax = pit.subplots(1, 1, figsize=(10, 5))
     sns.histplots(np.random.randn(100), ax=ax)
     st.pyplot(fig)
+
+
+
+with st.expander('User Profile:'):
+    st.text_input('Name:')
+    st.camera_input('camrta Input', key='camera_input')
 
 
